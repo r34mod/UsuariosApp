8d4e457576a2170f36a839601fe9d38327857703
r34mod <48959634+r34mod@users.noreply.github.com>
2021-10-28 16:12:52 +0200

    Add files via upload

---
+++ src/app/components/editar-perfil/editar-perfil.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from '../../services/user.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-editar-perfil',
+  templateUrl: './editar-perfil.component.html',
+  styleUrls: ['./editar-perfil.component.css'],
+  providers: [UsuarioService]
+})
+export class EditarPerfilComponent implements OnInit {
+
+  public id!: number;
+  public usuario!: Usuario;
+  public status!: string;
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _usuarioService: UsuarioService
+  ) { 
+    this.usuario = new Usuario(0,0,'','','','','','');
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void{
+    let cad = separarURL(this._router);
+    console.log(cad[2]+' / '+cad[3]);
+    if(cad[2]=='supervisores'){
+      if(this.usuario.rol == 'usuarios'){
+        this._usuarioService.updateUser(this.usuario._id,this.usuario).subscribe(data => {
+          console.log('creado '+data);
+          this._router.navigate(['/dashboard/'+cad[2]+'/'+cad[3]]);
+        });
+      }else{
+        alert("No tienes permisos para crear ese usuario");
+      }
+    }else if(cad[2]=='administradores'){
+      if(this.usuario.rol == 'supervisores'){
+        this._usuarioService.updateSuper(this.usuario._id,this.usuario).subscribe(data =>{
+          this._router.navigate(['/dashboard/'+cad[2]+'/'+cad[3]]);
+        })
+      }else if(this.usuario.rol == 'administradores'){
+        this._usuarioService.updateAdmin(this.usuario._id,this.usuario).subscribe(data =>{
+          this._router.navigate(['/dashboard/'+cad[2]+'/'+cad[3]]);
+        })
+      }
+    }
+  }
+
+  atras(): void {
+    var cad = separarURL(this._router);
+    this._router.navigate(['/dashboard'+'/'+cad[2]+'/'+cad[3]]);
+  }
+
+  
+
+}
+
+function separarURL(_router: Router) {
+  let direccion = _router.url;
+  let seperar = direccion.split("/");
+  return seperar;
+
+}
