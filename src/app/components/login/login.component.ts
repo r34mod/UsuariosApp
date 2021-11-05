b72f25c7e607a8844482c86e1733487f7b17062a
r34mod <48959634+r34mod@users.noreply.github.com>
2021-11-05 12:23:19 +0100

    Add files via upload

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,10 @@ import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario';
 import { UserLog } from 'src/app/models/userLog';
 import { UsuarioService } from 'src/app/services/user.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoginI } from 'src/app/models/login.interface';
+
 
 
 @Component({
@@ -15,100 +19,101 @@ export class LoginComponent implements OnInit {
 
   public variable!:any;
   userPadre!: UserLog;
-  user = new UserLog(0,"","");
+  
   public id!: string;
   public password!: string;
   public role!: string;
   public usuario!: Usuario;
+
+  private router!: Router;
+  private fb!: FormBuilder;
+  private auth!: AuthService;
+
+  form!: FormGroup;
+
+  userModulo = new UserLog("","","");
   
   constructor(
     private _usuarioService: UsuarioService,
-    private _router:Router
+    private _router:Router,
+    protected formBuilder: FormBuilder,
+    protected authen: AuthService
     
   ) {
-    
+      this.router=_router;
+      this.fb = formBuilder;
+      this.auth = authen;
+   }
+
+   get formLogin(){
+     return this.form.controls;
    }
 
   ngOnInit(): void {
+    this.initialForm();
+  }
+
+  login(){
+    console.log("Logueado");
+    
+    let idU = this.userModulo.id;
+    console.log(idU);
+    let pU = String(this.userModulo.password);
+    let rU = String(this.userModulo.role);
+    let user = { "id":idU, "password":pU, "role":rU};
+    //let user = { "id":1, "password":"1234", "role":"supervisores"};
+    if(rU == "usuarios" || rU == "supervisores" || rU == "administradores"){
+      let userJson = JSON.stringify(user);
+      var myObj = JSON.parse(userJson);
+      console.log(myObj);
+      this.auth.login(myObj);
+    }else{
+      alert("No existe este usuario...");
+      this._router.navigate(['/']);
+    }
    
   }
 
-
-  login():void{
-    console.log("entra");
-    
-    console.log(this.user.role);
-    switch(this.user.role){
-      
-      case 'usuarios':
-        this._usuarioService.getUserByUsername(this.user.id).subscribe(response => {
-          this.variable = response;
-
-          let datos = separarComas(JSON.stringify(this.variable));
-          let misDatos = devuelvoDatos(datos[0]);
-          console.log(misDatos);
-          if(misDatos.length > 1){
-            this._usuarioService.login(this.user.role, this.user.id).subscribe(data=>{
-              console.log(data);
-              this._usuarioService.setToken(data.token);
-              this._router.navigateByUrl('/dashboard/'+this.user.role+'/'+this.user.id);
-            });
-          }else{
-            alert("Error");
-          }
-        });
-        
-          
-           
-         
-        break;
-      case 'supervisores':
-        this._usuarioService.getSuperUserByUsername(this.user.id).subscribe(response=>{
-          this.variable = response;
-          let datos = separarComas(JSON.stringify(this.variable));
-          let misDatos = devuelvoDatos(datos[0]);
-          if(misDatos.length > 1){
-            this._usuarioService.login(this.user.role, this.user.id).subscribe(data=>{
-              console.log(data);
-              this._usuarioService.setToken(data.token);
-              this._router.navigateByUrl('/dashboard/'+this.user.role+'/'+this.user.id);
-            });
-          }
-        });
-        
-        break;
-      case 'administradores':
-        this._usuarioService.getAdminByUsername(this.user.id).subscribe(response=>{
-          this.variable = response;
-          let datos = separarComas(JSON.stringify(this.variable));
-          let misDatos = devuelvoDatos(datos[0]);
-          if(misDatos.length > 1){
-            this._usuarioService.login(this.user.role, this.user.id).subscribe(data=>{
-              console.log(data);
-              this._usuarioService.setToken(data.token);
-              this._router.navigateByUrl('/dashboard/'+this.user.role+'/'+this.user.id);
-            });
-          }
-        })
-       
-        break;
-
-    }
-    
+  onLogin(form:LoginI){
+    this.auth.loginByEmail(form).subscribe(data =>{
+      console.log(data);
+    })
   }
 
+  initialForm(): void {
+    this.form = new FormGroup({
+      id: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required),
+      role: new FormControl('',Validators.required)
+    });
+  }
+  
+    /*if(this.formLogin.id.status === 'INVALID' || this.formLogin.password.status === 'INVALID'){
+      return;
+    }
+
+    console.log(values);
+
+    this.auth.login(values.id, values.password).pipe(first()).subscribe(
+      response => {
+        console.log(response);
+        if(response){
+          localStorage.setItem('token', response);
+
+          this.router.navigate(['/dashboard']);
+        }
+      }
+    )*/
+  
+
+
+  
+    
+  
+
 }
 
 
-function separarComas(cadena: any) {
-  let separar = cadena.split("/");
-  return separar;
-}
 
 
-function devuelvoDatos(datos: Array<any>){
-  let nombre = datos.slice(42);
-  let cadena = (<any>nombre).split(",");
-  return cadena;
-}
 
