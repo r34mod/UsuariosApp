73bc6395adf6aa80caa159904384468c91854b75
r34mod <48959634+r34mod@users.noreply.github.com>
2021-11-05 12:26:12 +0100

    Add files via upload

--- src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -1,32 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario';
 import { UsuarioService } from 'src/app/services/user.service';
+import { ListaUsuarios } from 'src/app/models/usuario.interface';
+
 
 @Component({
   selector: 'app-lista-usuarios',
   templateUrl: './lista-usuarios.component.html',
   styleUrls: ['./lista-usuarios.component.css'],
   providers: [UsuarioService]
+  
 })
 export class ListaUsuariosComponent implements OnInit {
 
-  public usuarios!: Usuario[];
+  public variable!:any;
+  listaUsuario!: ListaUsuarios[];
+
+  @Input() usuario!: Usuario;
 
   constructor(
     private _usuarioService: UsuarioService
   ) { }
 
   ngOnInit(): void {
-    this._usuarioService.getAllUsers().subscribe(
-      response => {
-        if(response.usuarios){
-          this.usuarios = response.usuarios;
-          console.log(this.usuarios);
-        }else{
-          
-        }
+    let rolUsuario = getUsuario();
+     
+      if(rolUsuario=='usuarios'){
+        alert('Rol sin visualizacion');
+      }else if(rolUsuario== 'supervisores'){
+        this._usuarioService.getAllUsers().subscribe(data => {
+          this.listaUsuario = data;
+        });
+      }else if(rolUsuario== 'administradores'){
+        this._usuarioService.getSupervisores().subscribe(data => {
+          this.listaUsuario = data;
+        });
       }
-    )
   }
 
 }
+
+function getUsuario():string{
+  return decMD5(String(localStorage.getItem('usuario')));
+ }
+ 
+ function getId():string{
+   return decMD5(String(localStorage.getItem('idUsuario')));
+ }
+ 
+ 
+ function decMD5(cad:any){
+   return decodeURIComponent(escape(window.atob(cad)));
+ }
