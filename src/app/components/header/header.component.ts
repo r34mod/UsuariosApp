4dfe5eb9219fa8b10da8c3fed9d0c7f8b917ff04
r34mod <48959634+r34mod@users.noreply.github.com>
2021-11-05 12:25:46 +0100

    Add files via upload

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  url!:'http://localhost:8080/';
+
+  constructor( private router: Router,
+    private route: ActivatedRoute,
+     private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.logueado();
   }
 
+
+  logOut(){
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('usuario');
+    localStorage.removeItem('idUsuario');
+    this.router.navigateByUrl(this.url);
+  }
+
+  logueado(): boolean {
+    var si = false;
+    if(localStorage.getItem('auth_token')!==null){
+      si = true;
+    } 
+    return si;
+  }
 }
